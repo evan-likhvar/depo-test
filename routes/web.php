958ec6d0399763d2c557d7a21de7226209120959
Evan Likhvar <Evan.Likhvar@gmail.com>
2020-11-11 07:55:47 +0200

    refact landing

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,11 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
-Route::get('/wallet', [App\Http\Controllers\WalletController::class, 'edit'])->middleware('auth')->name('wallet.enter.get');
-Route::post('/wallet', [App\Http\Controllers\WalletController::class, 'update'])->middleware('auth')->name('wallet.enter.post');
-
-Route::get('/deposit', [App\Http\Controllers\DepositController::class, 'create'])->middleware('auth')->name('deposit.create.get');
-Route::post('/deposit', [App\Http\Controllers\DepositController::class, 'store'])->middleware('auth')->name('deposit.create.post');
-Route::get('/deposit/index', [App\Http\Controllers\DepositController::class, 'index'])->middleware('auth')->name('deposit.index');
-
-Route::get('/transaction/index', [App\Http\Controllers\TransactionController::class, 'index'])->middleware('auth')->name('transaction.index');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/wallet', [App\Http\Controllers\WalletController::class, 'edit'])->name('wallet.enter.get');
+    Route::post('/wallet', [App\Http\Controllers\WalletController::class, 'update'])->name('wallet.enter.post');
+    Route::get('/deposit', [App\Http\Controllers\DepositController::class, 'create'])->name('deposit.create.get');
+    Route::post('/deposit', [App\Http\Controllers\DepositController::class, 'store'])->name('deposit.create.post');
+    Route::get('/deposit/index', [App\Http\Controllers\DepositController::class, 'index'])->name('deposit.index');
+    Route::get('/transaction/index', [App\Http\Controllers\TransactionController::class, 'index'])->name('transaction.index');
+});
